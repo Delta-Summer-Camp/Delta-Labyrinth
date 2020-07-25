b88b15e9305b0b66b814723898cadb60e557f091
Vlad B <drvladb@gmail.com>
2020-07-24 21:59:39 -0400

    1. Add proper login/register 2. Split game into game.js 3. Added Game creation 4. Added Game joining 5. Sync Players between net games
    Checkout the progress: https://i.imgur.com/gr8zb2R.mp4

---
+++ public/js/game.js
@@ -0,0 +1,102 @@
+var player = document.getElementById("player")
+var colidables = document.getElementsByClassName("colidable")
+
+
+function updateplayer(way, newCords) {
+    var prev
+    var player = document.getElementById("player")
+    //console.log("moving player on screen")
+    if (way === "top") {
+        prev = player.style.top
+        player.style.top = newCords
+        //console.log(newCords)
+    } else {
+        prev = player.style.left
+        player.style.left = newCords
+    }
+
+    for (let i = 0; i < colidables.length; i++) {
+        if (colides(colidables[i])) {
+            console.log("colids")
+            if (way === "top") {
+                player.style.top = prev
+            } else {
+                player.style.left = prev
+            }
+            return false
+        }
+    }
+    return true
+}
+
+function colides(otherobject) {
+    var player = document.getElementById("player")
+    // Static Object
+    const othertop = parseInt(otherobject.style.top) + 50//top of the object (y)
+    const otherbottom = parseInt(otherobject.style.top) + parseInt(otherobject.style.height) + 50//bottom of the object(y)
+    const otherleft = parseInt(otherobject.style.left)//left of the object (x)
+    const otherright = parseInt(otherobject.style.left) + parseInt(otherobject.style.width)//right of the object (x)
+
+    // Player
+    const mytop = parseInt(player.style.top) //top of the player (y)
+    const mybottom = parseInt(player.style.top) + parseInt(player.style.height) //bottom of the player(y)
+    const myleft = parseInt(player.style.left) //left of the player (x)
+    const myright = parseInt(player.style.left) + parseInt(player.style.width) //right of the player (x)
+    return mybottom > othertop && mytop < otherbottom && myright > otherleft && myleft < otherright;
+
+}
+
+/*function gamestart() {
+    document.getElementById("coords").innerText = player.style.top + ", " + player.style.left
+    databasereference.set({
+        playerx: parseInt(player.style.top),
+        playery: parseInt(player.style.left),
+        playernick: globaluser.displayName
+    });
+}*/
+
+function updateloc() { // also serves as the page load
+    var player = document.getElementById("player")
+    document.getElementById("coords").innerText = player.style.top + ", " + player.style.left
+    // TODO send to firebase here
+    console.log(globaluser.displayName + ": x: " + player.style.top + ", y: " + player.style.left)
+    database.ref("games/" + sessionStorage.getItem("currentgame") + "/players/" + globaluser.uid).set({
+        playerx: parseInt(player.style.top),
+        playery: parseInt(player.style.left),
+        playernick: globaluser.displayName
+    });
+}
+
+document.addEventListener('keydown', function (event) {
+    if (isGameActive) {
+        var player = document.getElementById("player")
+        var sendcoords
+        if (event.code === 'KeyW') {
+            sendcoords = updateplayer("top", parseInt(player.style.top) - 10 + 'px')
+            if (sendcoords) {
+                updateloc()
+            }
+        }
+        if (event.code === 'KeyA') {
+            //player.style.left = parseInt(player.style.left) - 10 + 'px'
+            sendcoords = updateplayer("left", parseInt(player.style.left) - 10 + 'px')
+            if (sendcoords) {
+                updateloc()
+            }
+        }
+        if (event.code === 'KeyS') {
+            //player.style.top = parseInt(player.style.top) + 10 + 'px'
+            sendcoords = updateplayer("top", parseInt(player.style.top) + 10 + 'px')
+            if (sendcoords) {
+                updateloc()
+            }
+        }
+        if (event.code === 'KeyD') {
+            //player.style.left = parseInt(player.style.left) + 10 + 'px'
+            sendcoords = updateplayer("left", parseInt(player.style.left) + 10 + 'px')
+            if (sendcoords) {
+                updateloc()
+            }
+        }
+    }
+});
---
+++ public/js/gameinstatiation.js
@@ -0,0 +1,118 @@
+var isGameActive
+
+function joingame() {
+    if (sessionStorage.getItem("currentgame")) {
+        var gamecode = sessionStorage.getItem("currentgame")
+        var gamedirectory = database.ref("games")
+        gamedirectory.once('value').then(function (snapshot) {
+            //read games one
+            snapshot.forEach(function (childSnapshot) {
+                if (gamecode === childSnapshot.key) {
+                    alert("game has been found")
+                    var gamelink = database.ref("games/" + gamecode)
+                    sessionStorage.setItem("currentgame", gamecode)//dont know if we need this
+                    //game has been generated at this point
+                    document.getElementById("welcome").style.display = "none"
+                    document.getElementById("gamearea").style.display = "block"
+                    document.getElementById("coords").style.display = "block"
+                    isGameActive = true
+                    //return true
+                }
+                //alert("Game Not found, double check your code")
+            });
+        });
+    } else {
+
+        var gamecode = prompt("Please enter a game code: ")
+        var gamedirectory = database.ref("games")
+        gamedirectory.once('value').then(function (snapshot) {
+            //read games one
+            snapshot.forEach(function (childSnapshot) {
+                if (gamecode === childSnapshot.key) {
+                    alert("game has been found")
+                    var gamelink = database.ref("games/" + gamecode)
+                    sessionStorage.setItem("currentgame", gamecode)//dont know if we need this
+                    //game has been generated at this point
+                    document.getElementById("welcome").style.display = "none"
+                    document.getElementById("gamearea").style.display = "block"
+                    document.getElementById("coords").style.display = "block"
+                    isGameActive = true
+                    //return true
+                }
+                //alert("Game Not found, double check your code")
+            });
+        });
+    }
+    //add the player updating program
+    database.ref("games/" + gamecode + "/players").on('value', (snapshot) => {
+        console.log(snapshot.val())
+        //clear div
+        //otherplayerdiv.innerHTML = ""
+        otherplayers = document.getElementsByClassName("others")
+        for (let i = 0; i < otherplayers.length; i++) {
+            otherplayers[i].parentNode.removeChild(otherplayers[i])
+        }
+        //console.log(otherplayers[0])
+        //add players
+        var gamearea = document.getElementById("gamearea")//will not fix TODO ERROR
+        snapshot.forEach(function (childSnapshot) {
+            if (childSnapshot.key !== globaluser.uid) {
+                gamearea.innerHTML += "<div class=\"others\" id='" + childSnapshot.key + "' style=\"background-color: black; top: " + childSnapshot.val().playerx + "px; left: " + childSnapshot.val().playery + "px; width: 50px; height: 50px;\"></div>"
+                //"<p style=\"position: relative; top: " + childSnapshot.val().playerx + "px; left: " + childSnapshot.val().playery + "px;\">" + childSnapshot.val().playernick + "</p>" +
+
+                //otherplayerdiv.innerHTML += "<p>" + childSnapshot.val().displayname + ": x:" + childSnapshot.val().playerx + ", y:" + childSnapshot.val().playery + "</p>"
+                console.log(childSnapshot.val().playernick + ": x: " + parseInt(childSnapshot.val().playerx) + ", y: " + childSnapshot.val().playery)
+            }
+        });
+        //add the ondisconnect class TODO
+    }, (error) => {
+        console.error(error);
+    });
+}
+
+function creategame() {
+    var gamekey = Math.random().toString(36).substring(2, 15)
+    var gamelink = database.ref("games/" + gamekey)
+    gamelink.set({
+        //payload
+        owner: globaluser.uid,
+        gamemap: null, // pull gamelib here
+        players: null
+    })
+    alert("Game generated, the code is : #" + gamekey)
+    sessionStorage.setItem("currentgame", gamekey)//dont know if we need this
+    //game has been generated at this point
+    document.getElementById("welcome").style.display = "none"
+    document.getElementById("gamearea").style.display = "block"
+    document.getElementById("coords").style.display = "block"
+    //game show instatiation (load the game library)
+    //document.getElementById("body").innerHTML += "<script src=\"js/game.js\" onload='updateloc()'></script>"
+    isGameActive = true
+    //start the game loading
+    //updateloc()
+    //document.write('<script src="js/game.js"><\/script>');
+    database.ref("games/" + gamekey + "/players").on('value', (snapshot) => {
+        console.log(snapshot.val())
+        //clear div
+        //otherplayerdiv.innerHTML = ""
+        otherplayers = document.getElementsByClassName("others")
+        for (let i = 0; i < otherplayers.length; i++) {
+            otherplayers[i].parentNode.removeChild(otherplayers[i])
+        }
+        //console.log(otherplayers[0])
+        //add players
+        var gamearea = document.getElementById("gamearea")//will not fix TODO ERROR
+        snapshot.forEach(function (childSnapshot) {
+            if (childSnapshot.key !== globaluser.uid) {
+                gamearea.innerHTML += "<div class=\"others\" id='" + childSnapshot.key + "' style=\"background-color: black; top: " + childSnapshot.val().playerx + "px; left: " + childSnapshot.val().playery + "px; width: 50px; height: 50px;\"></div>"
+                //"<p style=\"position: relative; top: " + childSnapshot.val().playerx + "px; left: " + childSnapshot.val().playery + "px;\">" + childSnapshot.val().playernick + "</p>" +
+
+                //otherplayerdiv.innerHTML += "<p>" + childSnapshot.val().displayname + ": x:" + childSnapshot.val().playerx + ", y:" + childSnapshot.val().playery + "</p>"
+                console.log(childSnapshot.val().playernick + ": x: " + parseInt(childSnapshot.val().playerx) + ", y: " + childSnapshot.val().playery)
+            }
+        });
+        //add the ondisconnect class TODO
+    }, (error) => {
+        console.error(error);
+    });
+}
