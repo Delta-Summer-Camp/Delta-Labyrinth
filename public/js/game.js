db1a2b8ab42b6fcbf90cc6c71d347e274fc8c866
Vlad B <drvladb@gmail.com>
2020-07-26 11:44:47 -0400

    Patched Collision https://i.imgur.com/WthQej0.mp4

--- public/js/game.js
+++ public/js/game.js
@@ -32,16 +32,16 @@ function updateplayer(way, newCords) {
 function colides(otherobject) {
     var player = document.getElementById("player")
     // Static Object
-    const othertop = parseInt(otherobject.style.top)// + 50//top of the object (y)
-    const otherbottom = parseInt(otherobject.style.top) + parseInt(otherobject.style.height) + 50//bottom of the object(y)
-    const otherleft = parseInt(otherobject.style.left)//left of the object (x)
-    const otherright = parseInt(otherobject.style.left) + parseInt(otherobject.style.width)//right of the object (x)
+    const othertop = parseInt(otherobject.style.top) //+50//top of the object (y)
+    const otherbottom = parseInt(otherobject.style.top) + parseInt(otherobject.style.height)//+50//bottom of the object(y)
+    const otherleft = parseInt(otherobject.style.left)//+50//left of the object (x)
+    const otherright = parseInt(otherobject.style.left) + parseInt(otherobject.style.width)//+50//right of the object (x)
 
     // Player
-    const mytop = parseInt(player.style.top) //top of the player (y)
-    const mybottom = parseInt(player.style.top) + parseInt(player.style.height) //bottom of the player(y)
-    const myleft = parseInt(player.style.left) //left of the player (x)
-    const myright = parseInt(player.style.left) + parseInt(player.style.width) //right of the player (x)
+    const mytop = parseInt(player.style.top) + 50 //top of the player (y)
+    const mybottom = parseInt(player.style.top) + parseInt(player.style.height) + 50 //bottom of the player(y)
+    const myleft = parseInt(player.style.left) + 50 //left of the player (x)
+    const myright = parseInt(player.style.left) + parseInt(player.style.width) + 50 //right of the player (x)
     return mybottom > othertop && mytop < otherbottom && myright > otherleft && myleft < otherright;
 
 }
